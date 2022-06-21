1d902136e151d65e8a3e3ddbe285e5e3dd990947
Carlos Retamales <carlos.retamales.a@usach.cl>
2022-06-21 19:38:30 -0400

    [Carlos Retamales] - Regiones Dump inserción por medio de python

--- backend-ayni/src/main/java/cl/tbd/backendayni/BackendAyniApplication.java
+++ backend-ayni/src/main/java/cl/tbd/backendayni/BackendAyniApplication.java
@@ -1,5 +1,7 @@
 package cl.tbd.backendayni;
 
+import java.io.*;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +26,12 @@ public class BackendAyniApplication {
 				tablas.crearTablas();
 				System.out.println("| Se creo las Tablas de la Base de datos de Ayni |");
 				tablas.dump();
+				//Correr programa en python en la carpeta python para insertar los valores de las regiones
+				try{
+					Process p = Runtime.getRuntime().exec("python ../backend-ayni/src/main/python/insertar_datos_region.py");
+				}catch(IOException e){
+					System.out.println("| Error al correr el programa en python |");
+				}
 				System.out.println("| Se ingresaron los datos de la Base de datos de Ayni |");
 			}catch(Sql2oException e)
 			{
@@ -33,7 +41,7 @@ public class BackendAyniApplication {
 
         }
 		catch(Sql2oException e){
-			System.out.println("| No funciona |");
+			System.out.println("| No funciona, vea las credenciales y si la base de datos 'ayni' existe |");
 		}
 	}
 
