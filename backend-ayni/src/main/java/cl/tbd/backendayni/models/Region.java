d558afdbabc16c3f19da117cf72e116341c4d0d7
Carlos Retamales <carlos.retamales.a@usach.cl>
2022-06-21 20:40:57 -0400

    [Carlos Retamales] - Arreglos Region

--- backend-ayni/src/main/java/cl/tbd/backendayni/models/Region.java
+++ backend-ayni/src/main/java/cl/tbd/backendayni/models/Region.java
@@ -5,17 +5,17 @@ import org.postgis.PGgeometry;
 public class Region {
     private long id;
     private long id_region;
-    private String nom_region;
+    private String nom_reg;
     private PGgeometry geom;
 
     // CONSTRUCTOR REGION
     public Region() {
     }
 
-    public Region(long id, long id_region, String nom_region, PGgeometry geom) {
+    public Region(long id, long id_region, String nom_reg, PGgeometry geom) {
         this.id = id;
         this.id_region = id_region;
-        this.nom_region = nom_region;
+        this.nom_reg = nom_reg;
         this.geom = geom;
     }
 
@@ -28,8 +28,8 @@ public class Region {
         return id_region;
     }
 
-    public String getNom_region() {
-        return nom_region;
+    public String getNom_reg() {
+        return nom_reg;
     }
 
     public PGgeometry getGeom() {
@@ -46,8 +46,8 @@ public class Region {
         this.id_region = id_region;
     }
 
-    public void setNom_region(String nom_region) {
-        this.nom_region = nom_region;
+    public void setNom_reg(String nom_reg) {
+        this.nom_reg = nom_reg;
     }
 
     public void setGeom(PGgeometry geom) {
@@ -60,7 +60,7 @@ public class Region {
         return "Region{" +
                 "id=" + id +
                 ", id_region=" + id_region +
-                ", nom_region='" + nom_region + '\'' +
+                ", nom_reg='" + nom_reg + '\'' +
                 ", geom=" + geom +
                 '}';
     }
--- backend-ayni/src/main/java/cl/tbd/backendayni/repositories/RegionRepositoryImp.java
+++ backend-ayni/src/main/java/cl/tbd/backendayni/repositories/RegionRepositoryImp.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.sql2o.Connection;
 
+@Repository
 public class RegionRepositoryImp implements RegionRepository {
     @Autowired
     private Sql2o sql2o;
@@ -15,7 +16,7 @@ public class RegionRepositoryImp implements RegionRepository {
     @Override
     public int countRegiones() {
         int total = 0;
-        String sql = "SELECT COUNT(*) FROM region";
+        String sql = "SELECT COUNT(*) FROM regiones";
         try (Connection conn = sql2o.open()) {
             total = conn.createQuery(sql).executeScalar(Integer.class);
             return total;
@@ -25,7 +26,7 @@ public class RegionRepositoryImp implements RegionRepository {
     @Override
     public int newId() {
         int id = 0;
-        String sql = "SELECT MAX(id) FROM region";
+        String sql = "SELECT MAX(id) FROM regiones";
         try (Connection conn = sql2o.open()) {
             id = conn.createQuery(sql).executeScalar(Integer.class);
             return id;
@@ -35,7 +36,7 @@ public class RegionRepositoryImp implements RegionRepository {
     @Override
     public List<Region> getAll() {
         try (Connection conn = sql2o.open()) {
-            return conn.createQuery("SELECT * FROM region ORDER BY Region.id ASC")
+            return conn.createQuery("SELECT * FROM regiones ORDER BY regiones.id_region ASC")
                     .executeAndFetch(Region.class);
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -46,7 +47,7 @@ public class RegionRepositoryImp implements RegionRepository {
     @Override
     public List<Region> showRegionById(long id) {
         try (Connection conn = sql2o.open()) {
-            return conn.createQuery("SELECT * FROM region WHERE id = :id")
+            return conn.createQuery("SELECT * FROM regiones WHERE id = :id")
                     .addParameter("id", id)
                     .executeAndFetch(Region.class);
         } catch (Exception e) {
@@ -58,10 +59,10 @@ public class RegionRepositoryImp implements RegionRepository {
     @Override
     public Region createRegion(Region region) {
         try (Connection conn = sql2o.open()) {
-            String sql = "INSERT INTO region (id_region, nom_region, geom) VALUES (:id_region2, :nombre2, :geom2)";
+            String sql = "INSERT INTO regiones (id_region, nom_reg, geom) VALUES (:id_region2, :nombre2, :geom2)";
             conn.createQuery(sql)
                     .addParameter("id_region2", region.getId_region())
-                    .addParameter("nombre2", region.getNom_region())
+                    .addParameter("nombre2", region.getNom_reg())
                     .addParameter("geom2", region.getGeom())
                     .executeUpdate();
             region.setId(newId());
@@ -75,7 +76,7 @@ public class RegionRepositoryImp implements RegionRepository {
     @Override
     public void deleteRegionById(long id) {
         try (Connection conn = sql2o.open()) {
-            String sql = "DELETE FROM region WHERE id = :id";
+            String sql = "DELETE FROM regiones WHERE id = :id";
             conn.createQuery(sql)
                     .addParameter("id", id)
                     .executeUpdate();
@@ -88,10 +89,10 @@ public class RegionRepositoryImp implements RegionRepository {
     @Override
     public void updateRegion(Region region) {
         try (Connection conn = sql2o.open()) {
-            String sql = "UPDATE region SET id_region = :id_region2, nom_region = :nombre2, geom = :geom2 WHERE id = :id";
+            String sql = "UPDATE regiones SET id_region = :id_region2, nom_reg = :nombre2, geom = :geom2 WHERE id = :id";
             conn.createQuery(sql)
                     .addParameter("id_region2", region.getId_region())
-                    .addParameter("nombre2", region.getNom_region())
+                    .addParameter("nombre2", region.getNom_reg())
                     .addParameter("geom2", region.getGeom())
                     .addParameter("id", region.getId())
                     .executeUpdate();
